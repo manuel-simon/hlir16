6581b58fe2cc7e0977d449e9b0615d402bc845e3
Kitlei Róbert <kitlei@elte.hu>
2019-12-19 11:58:31 +0100

    Add convenience functionality: representation, walking

--- p4node.py
+++ p4node.py
@@ -83,8 +83,10 @@ class P4Node(object):
         "declid",
 
         # common tools
+        "all_nodes_by_type",
         "paths_to",
         "by_type",
+        "json_repr",
     }
 
     def __init__(self, dict={}, vec=None):
@@ -94,12 +96,15 @@ class P4Node(object):
         self._data = {}
         self.vec = vec
 
-    def __str__(self, show_name=True, show_type=True, show_funs=True, details=True, show_colours=True):
+    def __str__(self, show_name=True, show_type=True, show_funs=True, details=True, show_colours=True, depth=0):
         """A textual representation of a P4 HLIR node."""
         if self.vec is not None:
             if len(self.vec) > 0 and type(self.vec[0]) is P4Node:
                 fmt   = '{{0:>{}}} {{1}}'.format(len(str(len(self.vec))))
-                return '\n'.join([fmt.format(idx, elem) for idx, elem in enumerate(self.vec)])
+                if type(self.vec) is dict:
+                    return '\n'.join([fmt.format(key, self.vec[key]) for key in sorted(self.vec.keys())])
+                else:
+                    return '\n'.join([fmt.format(idx, elem) for idx, elem in enumerate(self.vec)])
             return str(self.vec)
 
         name = self.name if hasattr(self, 'name') else ""
@@ -107,9 +112,10 @@ class P4Node(object):
         part1 = name if show_name else ""
         part2 = "#" + str(self.get_attr('Node_ID'))
         part3 = "#{}".format(self.node_type) if show_type else ""
-        part4 = "[{}]".format(', '.join(self.xdir(details))) if show_funs else ""
+        part4 = "[{}]".format(', '.join(self.xdir(details, depth=depth))) if show_funs else ""
 
-        return "{}{}{}{}".format(part1, _c(part2, clr_nodeid, show_colours), _c(part3, clr_nodetype, show_colours), part4)
+        indent = " " * (8*depth)
+        return "{}{}{}{}{}".format(indent, part1, _c(part2, clr_nodeid, show_colours), _c(part3, clr_nodetype, show_colours), part4)
 
     def __repr__(self):
         return self.__str__()
@@ -128,7 +134,7 @@ class P4Node(object):
         return P4Node({}, [node for node in self.vec if node.node_type == key])
 
     def __len__(self):
-        if not self.vec:
+        if self.vec is None:
             return 0
         return len(self.vec)
 
@@ -136,6 +142,53 @@ class P4Node(object):
         if self.vec is not None:
             for x in self.vec:
                 yield x
+
+    def __mod__(self, other):
+        return self.paths_to(other)
+
+    def __lt__(self, depth):
+        """This is not a proper comparison operator.
+        Rather, it pretty prints the node to the standard output.
+        You can use it as the postfix "love operator" on a node: `node<3` """
+        import json
+        from ruamel import yaml
+        import re
+
+        depth = max(1, depth)
+
+        dumped = yaml.dump(yaml.safe_load(json.dumps(self.json_repr(depth))), default_flow_style=False).decode('string_escape')
+        ascii_escape = '\033'
+        print(re.sub(r'\\e[ ]*', ascii_escape, re.sub(r'\"\\e', '\\e', re.sub(r'\\e\[0m\"', '\\e[0m', dumped))))
+
+        return None
+
+    def __nonzero__(self):
+        return 'node_type' in self.__dict__ and self.__dict__['node_type'] != "INVALID"
+
+    def json_repr(self, depth=3, max_vector_len=lambda depth: 2 if depth > 2 or depth <= 0 else [8, 4][depth - 1], is_top_level = True):
+        if depth <= 0:
+            return "..."
+
+        if self.is_vec():
+            maxlen = max_vector_len(depth)
+            selflen = len(self.vec)
+            repr = [e.json_repr(depth, is_top_level = True) if type(e) is P4Node else e for e in list(self.vec)[:maxlen]]
+            if selflen > maxlen:
+                repr += ["({} more elements, {} in total)".format(selflen - maxlen, selflen)]
+        else:
+            repr = {}
+            for d in self.xdir(details=False, show_colours=False):
+                reprattrname = _c("." + d, clr_attrname)
+                reprtype = _c("#" + str(self.node_type) if 'node_type' in self.__dict__ else "#", clr_nodetype)
+                reprfld = "{}{}".format(reprattrname, reprtype)
+                if type(self.get_attr(d)) is P4Node:
+                    repr[reprfld] = self.get_attr(d).json_repr(depth-1, is_top_level = False)
+                else:
+                    repr[reprfld] = str(self.get_attr(d))
+
+        nodename = "{}{}".format(_c(self.name  if 'name' in self.__dict__ else "", clr_value), _c("#" + str(self.node_type) if 'node_type' in self.__dict__ else "#", clr_nodetype))
+
+        return { nodename: repr } if is_top_level else repr
 
     def remove_attr(self, key):
         del self.__dict__[key]
@@ -158,13 +211,53 @@ class P4Node(object):
         self.vec.append(elem)
 
     def __add__(self, other):
-        """Adds elements to the vector of the object."""
+        """Returns a P4Node that contains the elements from the node's vector and the other list/P4Node."""
         if type(other) is list:
             return P4Node({}, self.vec + other)
         return P4Node({}, self.vec + other.vec)
 
-    def __call__(self, key):
-        return self.__dict__[key] if key in self.__dict__ else self
+    def __getattr__(self, key):
+        if key.startswith('__') or key == 'vec':
+            return object.__getattr__(self, key)
+
+        if key.startswith('_'):
+            realkey = key[1:]
+            return self.__dict__[realkey] if realkey in self.__dict__ else self
+
+        if self.__dict__['node_type'] == "INVALID":
+            return self
+
+        return self.__dict__[key]
+
+
+    def __call__(self, key, continuation = None, default = None):
+        """The key is a dot separated sequence of attributes such as 'type.type_ref.name'.
+        If the attributes can be traversed, the node that is reached is returned.
+        If the attribute sequence is broken, a P4 node describing the failure
+        (or the default parameter, if it is set) is returned."""
+        if self.node_type == "INVALID":
+            return self
+
+        original_node = self
+
+        current_node = self
+        for idx, k in enumerate(key.split(".")):
+            if k not in current_node.__dict__:
+                retval = P4Node()
+                retval.name = "INVALID"
+                retval.node_type = "INVALID"
+                retval.original_node = original_node
+                retval.original_path = key
+                retval.last_good_node = current_node
+                retval.remaining_path = ".".join(key.split(".")[idx:])
+                return retval
+
+            current_node = current_node.__dict__[k]
+
+        if current_node:
+            return continuation(current_node) if callable(continuation) else current_node
+
+        return default() if callable(default) else default or current_node
 
     def set_vec(self, vec):
         """Sets the vector of the object."""
@@ -173,7 +266,7 @@ class P4Node(object):
     def is_vec(self):
         return self.vec is not None
 
-    def xdir(self, details=False, show_colours=True):
+    def xdir(self, details=False, show_colours=True, depth=0):
         """Lists the noncommon attributes of the node."""
         def follow_path(node, path):
             for pathelem in path:
@@ -186,16 +279,26 @@ class P4Node(object):
 
         def follow_paths(attrname, node):
             paths = [
-                ('expr', ['path', 'name']),
-                ('expr', ['ref', 'name']),
-                ('expr', ['expr', 'ref', 'name']),
-                ('expr', ['member', 'member']),
-                ('expr', ['member']),
-                ('path', ['name']),
-                ('ref',  ['name']),
-                ('member',  ['member']),
+                'header_ref.name',
+                'type_ref.path.name',
+                'type_ref.name',
+                'type.path.node_type',
+                'type.path.name',
+                'type.name',
+                'type.node_type',
+                'baseType.path.name',
+                'field_ref.name',
+                'expr.path.name',
+                'method.path.name',
+                'expr.ref.name',
+                'expr.expr.ref.name',
+                'expr.member.member',
+                'expr.member',
+                'path.name',
+                'ref.name',
+                'member.member',
             ]
-            for path in [p for (a, p) in paths if a == attrname]:
+            for path in [p.split('.')[1:] for p in paths if p.split('.')[0] == attrname]:
                 result = follow_path(node, path)
                 if result is not None:
                     return result
@@ -211,7 +314,7 @@ class P4Node(object):
                 return (True, "=" + _c("fun", clr_function))
 
             if type(attr) is not P4Node:
-                return (True, "=" + _c(str(attr), clr_value))
+                return (True, "=" + _c(str(attr) or '""', clr_value))
 
             result = follow_paths(d, attr)
             if result is not None:
@@ -220,10 +323,13 @@ class P4Node(object):
             if type(attr.get_attr(d)) is P4Node and attr.get_attr(d).vec is not None:
                 attrlen = len(attr.get_attr(d).vec)
                 return (attrlen > 0, "**" + _c(str(attrlen), clr_attrmul if attrlen > 0 else clr_off, show_colours))
-            if attr.vec is None:
+
+            listables = ['NameMap', 'ParameterList', 'Annotations', 'Vector']
+            is_listable = [attr.node_type.startswith(t) for t in listables if hasattr(attr, 'node_type')] != []
+            if attr.vec is None and not is_listable:
                 return (True, "")
 
-            attrlen = len(attr.vec)
+            attrlen = len(attr.vec or [])
             return (attrlen > 0, "*" + _c(str(attrlen), clr_attrmul if attrlen > 0 else clr_off, show_colours))
 
         return [_c(d, clr_attrname if is_clr_on else clr_off, show_colours) + attr_details
@@ -232,8 +338,8 @@ class P4Node(object):
                     if d not in P4Node.common_attrs
                     for (is_clr_on, attr_details) in [show_details(d)] ]
 
-    def str(self, show_name=True, show_type=True, show_funs=True):
-        return P4Node.__str__(self, show_name, show_type, show_funs)
+    def str(self, show_name=True, show_type=True, show_funs=True, details=True, show_colours=True, depth=0):
+        return P4Node.__str__(self, show_name, show_type, show_funs, details, show_colours, depth)
 
     def get(self, name, type_name=None, cond=lambda elem: True):
         """A convenient way to get the element with the given name (and type, if given) in a vector.
